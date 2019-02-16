4b455c464d89945bd4c76e2aff73d4ce89bef0ca
Lee Jeon Hyeong <jhlove0490@gmail.com>
2019-02-16 15:09:31 +0900

    final & static keyword example

---
+++ testex/src/com/javalec/staticex/MainClass.java
@@ -0,0 +1,9 @@
+package com.javalec.staticex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println(PIClass.PI);
+	}
+}
---
+++ testex/src/com/javalec/staticex/PIClass.java
@@ -0,0 +1,10 @@
+package com.javalec.staticex;
+
+public class PIClass {
+
+	public static final double PI = 3.14D; // final은 더이상 변경하지 않게 하는 keyword
+	
+	public PIClass() {
+		// TODO Auto-generated constructor stub
+	}
+}
